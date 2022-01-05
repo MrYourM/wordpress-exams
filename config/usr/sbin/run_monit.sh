b597be6c8dbec7577a80a6d84db2e4c0deeeba73
youlizhu <youlizhu@metadig.com>
2022-01-05 06:47:49 +0000

    add 0700 to monitrc

--- config/usr/sbin/run_monit.sh
+++ config/usr/sbin/run_monit.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 wordpress_path="/var/www/html"
-mkdir -p $wordpress_path
 grep -Eq "^open_basedir = " /etc/php/7.4/fpm/php.ini || echo "open_basedir = $wordpress_path" | tee -a /etc/php/7.4/fpm/php.ini
 
+chmod 0700 /etc/monit/monitrc
 chown -R www-data:www-data $wordpress_path
 chmod -R 755 $wordpress_path
+
 
 /usr/bin/monit -c /etc/monit/monitrc -I
